e8e8c6a1d91cabafed3c588c859f187fd5b2cae9
jump3r <anton.bondar1@gmail.com>
2014-12-16 02:32:52 -0500

    Extension context menu skeleton

---
+++ Summarize-It-Extension/summarizeit.js
@@ -0,0 +1,46 @@
+
+/*
+Example info
+
+info: {
+"editable":false,
+"menuItemId":"context:selection",
+"pageUrl":"https://www.google.ca/search?q=wassily+kandinsky+famous+paintings&hl=en&biw…bih=697&source=lnms&sa=X&ei=GNWPVLnAJpOYyQTXxIKADA&ved=0CAUQ_AUoAA&dpr=1.1",
+"selectionText":"esionistic landscape, which already marked the slow transition into abstraction. Wassily Ka"
+}
+
+tab: {
+"active":true,
+"favIconUrl":"https://www.google.ca/favicon.ico",
+"height":767,
+"highlighted":true,
+"id":313,
+"incognito":false,
+"index":7,
+"openerTabId":307,
+"pinned":false,
+"selected":true,
+"status":"complete",
+"title":"wassily kandinsky famous paintings - Google Search",
+"url":"https://www.google.ca/search?q=wassily+kandinsky+famous+paintings&hl=en&biw…bih=697&source=lnms&sa=X&ei=GNWPVLnAJpOYyQTXxIKADA&ved=0CAUQ_AUoAA&dpr=1.1",
+"width":1600,
+"windowId":1}
+
+*/
+function onClickHandler(info, tab) {
+  if (info.menuItemId == "context:selection") {        
+      //console.log(info.selectionText);
+      console.log(info["selectionText"]);
+  }
+
+};
+
+chrome.runtime.onInstalled.addListener(function() {
+
+  chrome.contextMenus.create({"title": "Summarize it!", "contexts":["selection"], "id": "context:" + "selection"});
+  chrome.contextMenus.create({"title": "Select Text To Summarize it!", "id": "NoSelection"});
+
+});
+
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
